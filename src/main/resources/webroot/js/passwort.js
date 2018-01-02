62ee72d11015148208af8c9304bd998042acd1b3
blank_adm <blank_adm@BLANK-L01>
2018-01-02 17:48:44 +0100

    Passwort vergessen und Checkbox hinzugefügt

--- src/main/resources/webroot/js/passwort.js
+++ src/main/resources/webroot/js/passwort.js
@@ -34,8 +34,8 @@ $(document).ready(function () {
                 .append("neuer Benutzername: <input type='text' id='benutzername'/><br>\n")
                 .append("gewünschtes Passwort: <input type='password' id='passwort1'/><br>\n")
                 .append("Passwort wiederholen: <input type='password' id='passwort2'/><br>\n")
-                .append("<input type='button' value='Benutzer erstellen' id='benutzererstellenknopf'/>");
-
+                .append("<input type='button' value='Benutzer erstellen' id='benutzererstellenknopf'/>")
+.append("<input type='checkbox' name='checkknopf' id='checkknopf'/>"+"Ich bin freiwillig hier");
     });
 
     $(document).on("click", "#benutzererstellenknopf", function () {
@@ -45,8 +45,8 @@ $(document).ready(function () {
             $("body").html("Benutzer erstellt: " + $("#benutzername").val() + " Passwort: " + $("#passwort1").val() + " <br>")
                     .append("Einloggen<br>\nName: <input type='text' id='anmeldename'/><br>")
                     .append("Passwort: <input type='password' id='passwort'/><br>\n")
-                    .append("<input type='button' value='OK' id='anmeldeknopf'/>\n")
-                    .append("<input type='button' value='Registrieren' id='registrierungsknopf'/><br>\n");
+                    .append("<input type='button' value='OK' id='anmeldeknopf'/>\n");
+                    
 
 
 
@@ -54,7 +54,8 @@ $(document).ready(function () {
         } else {
             $("body").append("<br>Passwort bitte überprüfen");
         }
-    });
+   
+});
 
 
     $.post("../anfrage",
@@ -70,6 +71,7 @@ $(document).ready(function () {
                                 .append("Passwort: <input type='password' id='passwort'/><br>\n")
                                 .append("<input type='button' value='OK' id='anmeldeknopf'/>\n")
                                 .append("<input type='button' value='Registrieren' id='registrierungsknopf'/><br>\n")
+                        .append("<input type='button' value='Passwort vergessen?' id='vergessenknopf'/><br>\n");
                     } else {
                         $("body").html("Gratulation, du bist angemeldet!")
                                 .append("<br><input type='button' value='logout' id='logout'/>");
@@ -77,5 +79,8 @@ $(document).ready(function () {
                 }
             }
     );
-
+$(document).on("click", "#vergessenknopf", function () {
+    $("body").html("Leider können wir die Tatsache nicht ändern, dass Sie ihr Passwort vergessen haben. Hier zur Aufmunterung ein kleines Bild")
+            .append("<br><input type='image' src='https://i.pinimg.com/736x/2c/d8/77/2cd8770926c40117f5660d73ad1daace--facebook-com.jpg'/>");
+});
 });
